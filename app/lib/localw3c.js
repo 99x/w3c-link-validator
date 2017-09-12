7edb7981333360c02152a4fcc210882097a0028e
Shalitha Suranga <shalithasuranga@gmail.com>
2017-09-12 20:40:31 +0530

    Removed some unwanted code

--- app/lib/localw3c.js
+++ app/lib/localw3c.js
@@ -1,4 +1,5 @@
 const url = require('url');
+const path = require('path');
 const request = require('request');
 const cheerio = require('cheerio');
 const chalk = require('chalk');
@@ -19,15 +20,6 @@ var isLocal = function (link) {
 };
 
 
-var isAbsolute = function (link) {
-    return link.indexOf('http://') === 0 || link.indexOf('https://') === 0;
-};
-
-var makeLinkFromRelative = function (root,link) {
-    return  root + '/'+ link;
-};
-
-
 var initValidator = function (options) {
     if(typeof globalOptions.localUrl != 'undefined') {
         globalOptions.localUrl = options.localUrl;
@@ -41,29 +33,48 @@ var initValidator = function (options) {
 var runValidator =  function(rootUrl){
     if(typeof rootUrl == 'undefined')
         rootUrl = globalOptions.localUrl;
-    console.log(chalk.green.bold('PARSING ROOT '),chalk.white(rootUrl));
+
+    var urlinfo = url.parse(rootUrl);
+    console.log(urlinfo);
+    var _link = urlinfo.href;
+    var _base = path.basename(urlinfo.pathname);
+
+
+    console.log(chalk.green.bold('VALIDATION RUNNING ON '));
+    console.log('URL : ' + _link);
+    console.log('base : ' + _base);
 
 
     request(rootUrl, function (error, response, body) {
         var $ = cheerio.load(body);
         var links = $('a');
+
         links.each(function (i, elm) {
             if (typeof elm.attribs != 'undefined') {
                 if (typeof elm.attribs.href != 'undefined') {
                     var innerLink = elm.attribs.href;
-
                     if(isLocal(innerLink)){
-                        if(!isAbsolute(innerLink))
-                            innerLink = makeLinkFromRelative(rootUrl,innerLink);
+                        //if(!isAbsolute(innerLink))
+                            innerLink = url.resolve(rootUrl,innerLink);
                         request(innerLink, function (error, response, body) {
-                           if(!error)
-                                console.log('LINK ',chalk.yellow(innerLink) +' '+ _status.showStatus(response.statusCode));
+                           if(!error) {
+                               console.log('LINK ', chalk.yellow(innerLink) + ' ' + _status.showStatus(response.statusCode));
+                               if(response.statusCode != 200){
+                                   console.log(chalk.red('Problem on '));
+                                   console.log(chalk.white($.html(elm)));
+                               }
+                           }
                         });
                     }
                     else{
                         request(innerLink, function (error, response, body) {
-                            if(!error)
-                                console.log('LINK ',chalk.yellow(innerLink) +' '+ _status.showStatus(response.statusCode));
+                            if(!error) {
+                                console.log('LINK ', chalk.yellow(innerLink) + ' ' + _status.showStatus(response.statusCode));
+                                if(response.statusCode != 200){
+                                    console.log(chalk.red('Problem on '));
+                                    console.log(chalk.white($.html(elm)));
+                                }
+                            }
                         });
                     }
                 }
@@ -81,3 +92,7 @@ var runValidator =  function(rootUrl){
 
 exports.init = initValidator;
 exports.run = runValidator;
+
+
+initValidator({localUrl : 'http://localhost:80/w3ctest/'});
+runValidator(globalOptions.localUrl);
