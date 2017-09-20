65c8b36a30ac79fc80e5cc410dd9972f05694c18
Shalitha Suranga <shalithasuranga@gmail.com>
2017-09-20 18:44:02 +0530

    Extra newline by console.log removed

--- src/links.js
+++ src/links.js
@@ -28,7 +28,7 @@ var linkChecker = function ($, rootUrl) {
 
     var urls = [];
     if(links.length == 0){
-        console.log();
+
     }
     else {
         console.log(chalk.italic('Adding ' + links.length+ ' link(s) to the queue...\n'));
